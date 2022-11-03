667fec602ca8ee06c89e44cd884682b0d9382938
alifdevz <alifm.devz@gmail.com>
2022-11-03 20:11:09 +0800

    Set maximum characters for title + Make the note textarea can be filled with characters

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -9,20 +9,42 @@ class NoteInput extends React.Component {
             title: '',
             body: '',
             createdAt: +new Date(),
-            archived: false
+            archived: false,
+            availableTitleChars: 50
         }
 
         this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+        this.onCharLimitChangeEventHandler = this.onCharLimitChangeEventHandler.bind(this);
         this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
         this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
     }
 
-    onTitleChangeEventHandler(event) {
+    onCharLimitChangeEventHandler(event) {
+        let chars = event.target.value;
+        let difference = 50 - chars.length;
+        if (difference >= 0) {
+            this.setState(
+                {
+                    availableTitleChars: difference
+                } 
+            );
+        }
+    }
 
+    onTitleChangeEventHandler(event) {
+        let charsLength = event.target.value.length;
+        if (charsLength <= 50) {
+            // console.log(this.state.availableTitleChars, event.target.value, charsLength)
+            this.setState({
+                title: event.target.value
+            });
+        }
     }
 
     onBodyChangeEventHandler(event) {
-
+        this.setState({
+            body:event.target.value
+        });
     }
     
     onSubmitEventHandler(event) {
@@ -34,9 +56,16 @@ class NoteInput extends React.Component {
             <div className="note-form">
                 <h2>Buat Catatan</h2>
                 <br />
-                <span className="characters-left">Sisa karakter: {50}</span>
+                <span className="characters-left">Sisa karakter: {this.state.availableTitleChars}</span>
                 <form onSubmit={this.onSubmitEventHandler} >
-                    <input placeholder="Ini adalah judul..." value={this.state.title} onChange={this.onTitleChangeEventHandler} />
+                    <input 
+                        placeholder="Ini adalah judul..."
+                        value={this.state.title}
+                        onChange={(event) =>{
+                            this.onTitleChangeEventHandler(event);
+                            this.onCharLimitChangeEventHandler(event);
+                        }}
+                    />
                     <textarea placeholder="Tulislah catatanmu di sini..." value={this.state.body} onChange={this.onBodyChangeEventHandler} />
                     <button type="submit">Buat</button>
                 </form>
