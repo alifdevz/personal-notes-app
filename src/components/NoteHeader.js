933e89bbba98d41e43682e5ec4bdfc11c1f9cace
alifdevz <alifm.devz@gmail.com>
2022-11-03 12:59:58 +0800

    Add NoteHeader component

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -1,2 +1,17 @@
 import React from "react";
 
+function NoteHeader() {
+    return (
+        <div className="header">
+            <div className="note-and-search">
+                <h1>Note</h1>
+                <div className="note-search-wrapper">
+                    <input className="note-search" type="text" placeholder="Cari catatan..." />
+                </div>
+            </div>
+            <hr className="header-line"/>
+        </div>
+    )
+}
+
+export default NoteHeader;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import NoteHeader from './components/NoteHeader';
 import NoteInput from './components/NoteInput';
 import './styles/style.css';
 import { getInitialData, showFormattedDate } from './utils/index';
@@ -8,10 +9,5 @@ import { getInitialData, showFormattedDate } from './utils/index';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <div className="note-app__header">
-        
-        <h1 className='.note-app__header h1'>Note</h1>
-        <NoteInput />
-        
-    </div>
+    <NoteHeader />
 );
