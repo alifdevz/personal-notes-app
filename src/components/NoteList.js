9d7f056f76a4ed4a0c778e082d49ad550c770a7b
alifdevz <alifm.devz@gmail.com>
2022-11-05 16:29:54 +0800

    Add 'Catatan Aktif' title to the active notes

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,6 +5,7 @@ function NoteList({ notes }) {
     // console.log('notelist', notes)
     return (
         <div className="note-list">
+            <h2>Catatan Aktif</h2>
             <div className="grid-container">
                 {notes.map(note => <NoteItem key={note.id} {...note} />)}
             </div>
