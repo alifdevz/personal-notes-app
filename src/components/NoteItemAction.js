098516f75db6ab0866cb40548035b91020c18631
alifdevz <alifm.devz@gmail.com>
2022-11-06 14:28:21 +0800

    Change NoteItemAction from class component to functional component

--- src/components/NoteItemAction.js
+++ src/components/NoteItemAction.js
@@ -1,18 +1,12 @@
 import React from "react";
 
-class NoteItemAction extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="note-item__action">
-                
-                
-            </div>
-        )
-    }
+function NoteItemAction() {
+    return (
+        <div className="note-item__action">
+            
+            
+        </div>
+    )
 }
 
 export default NoteItemAction;
