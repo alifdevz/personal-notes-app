231a55d52ecd01dc978156b8a81985504a4e5504
alifdevz <alifm.devz@gmail.com>
2022-11-06 14:31:31 +0800

    Add NoteItemDeleteButton and NoteItemArchiveButton components inside NoteItemAction component

--- src/components/NoteItemAction.js
+++ src/components/NoteItemAction.js
@@ -1,10 +1,12 @@
 import React from "react";
+import NoteItemArchiveButton from "./NoteItemArchiveButton";
+import NoteItemDeleteButton from "./NoteItemDeleteButton";
 
-function NoteItemAction() {
+function NoteItemAction({ id, onDelete }) {
     return (
         <div className="note-item__action">
-            
-            
+            <NoteItemDeleteButton id={id} onDelete={onDelete} />
+            <NoteItemArchiveButton />
         </div>
     )
 }
--- src/components/NoteItemArchiveButton.js
+++ src/components/NoteItemArchiveButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NoteItemArchiveButton() {
-    render (
+    return (
         <button className="note-item__archive-button">Arsipkan</button>
     )
 }
